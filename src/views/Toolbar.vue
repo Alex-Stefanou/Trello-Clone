a4136f7329563cd3c7c000fa1fe640b8d53ad59f
Alex Stefanou <Alex-Stefanou@users.noreply.github.com>
2020-05-19 14:07:53 +0100

    Added Elements to toolbar

--- src/views/Toolbar.vue
+++ src/views/Toolbar.vue
@@ -1,6 +1,41 @@
 <template>
-    <div>
-        Toolbar goes here
+    <div class="navbar is-transparent">
+        <div class="navbar-menu">
+
+            <div class="navbar-start">
+                <button class="button">
+                    boardName
+                </button>
+                <button class="button">
+                    <i class="far fa-star"></i>
+                </button>
+                |
+                <button class="button">
+                    Personal
+                </button>
+                |
+                <button class="button">
+                    <i class="fas fa-lock"></i> Private
+                </button>
+                |
+                <button class="button">
+                    <i class="fas fa-user-circle"></i>
+                </button>
+                <button class="button">
+                    Invite
+                </button>
+            </div>
+
+            <div class="navbar-end">
+                <button class="button">
+                    <i class="fas fa-concierge-bell"></i> Butler
+                </button>
+                <button class="button">
+                    <i class="fas fa-ellipsis-h"></i> Show Menu
+                </button>
+            </div>
+
+        </div>
     </div>
 </template>
 
@@ -11,4 +46,8 @@ export default {
 </script>
 
 <style scoped>
+.navbar {
+    background-color: transparent;
+    min-height: 3rem;
+}
 </style>
